b70bef8af0e4bb6eaf630a492f0b9534e80e1476
Ian-2INFO3 <ianrobertoestudos10@gmail.com>
2022-06-06 10:39:26 -0300

    table adicionada

--- src/views/TimesView.vue
+++ src/views/TimesView.vue
@@ -7,5 +7,33 @@
       <input type="text" />
       <button>Salvar</button>
     </div>
+    <div class="list-times">
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Time 1</td>
+            <td>???</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Time 2</td>
+            <td>???</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Time 3</td>
+            <td>???</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
