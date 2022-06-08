b5285bc4f53efdb165795b56cf00675d42f58d10
gustavokubiack <guukubiack@gmail.com>
2022-06-08 14:13:25 -0300

    jogadores funcionando com input

--- src/views/JogadoresView.vue
+++ src/views/JogadoresView.vue
@@ -1,3 +1,44 @@
 <template>
-  <main>Jogadores</main>
+  <div class="container">
+    <div class="title">
+      <h2>Gerenciamento de Jogadores</h2>
+    </div>
+    <div class="form-input">
+      <input type="text" placeholder="Nome do Jogador" />
+      <input type="text" placeholder="Nome do time" />
+      <button>Salvar</button>
+    </div>
+    <div class="list-times">
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Time-Id</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Afonso</td>
+            <th>001</th>
+            <td>???</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Lucas</td>
+            <th>002</th>
+            <td>???</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Afonso</td>
+            <th>003</th>
+            <td>???</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
