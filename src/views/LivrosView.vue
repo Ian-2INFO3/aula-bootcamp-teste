60346d2b960e32e8ff6e2223cbe56333a419ecff
Ian-2INFO3 <ianrobertoestudos10@gmail.com>
2022-06-29 15:10:44 -0300

    funcionando

--- src/views/LivrosView.vue
+++ src/views/LivrosView.vue
@@ -10,6 +10,7 @@ export default {
           id_editora: "630c1d03-cb59-460a-89b8-cca1d42c1bc6",
           id_autor: "4b95ec3f-c3a0-49d9-971d-b3696657fbbd",
           id_cartegoria: "16390338-fb77-4e4c-84f9-da9d1a1bdee7",
+          preco_livro: "R$ 70,00",
         },
         {
           id_livro: "736c8f35-b5cf-43bf-a9ee-e4d8950ee0a5",
@@ -17,6 +18,7 @@ export default {
           id_editora: "597ceef8-36ac-4b0e-859e-77551c9818c0",
           id_autor: "ec38e964-9ddc-4d39-b6e3-688e239f8da9",
           id_cartegoria: "933f3ba7-0a49-4e63-80c3-134600987018",
+          preco_livro: "R$ 40,00",
         },
         {
           id_livro: "519d6a87-60c2-465a-aa2f-1b8d0ef079cf",
@@ -24,26 +26,41 @@ export default {
           id_editora: "8ecb8a1e-5114-496d-9bee-1308dccf4b87",
           id_autor: "d48aca58-0c61-46fc-896f-9ea630a90cfb",
           id_cartegoria: "ed4a44b6-720b-4b7b-a4fa-a2ff27ec3d17",
+          preco_livro: "R$ 20,00",
         },
       ],
       novo_livro: "",
+      novo_preco: "",
+      editora: "",
+      cartegoria: "",
+      autor: "",
     };
   },
   methods: {
     salvar() {
-      if (this.novo_livro !== "") {
+      if (
+        this.novo_livro &&
+        this.novo_preco &&
+        this.editora &&
+        this.autor &&
+        this.cartegoria !== ""
+      ) {
         const novo_idlivro = uuidv4();
-        const novo_ideditora = uuidv4();
         const novo_idautor = uuidv4();
         const novo_idcartegoria = uuidv4();
         this.livros.push({
           id_livro: novo_idlivro,
-          id_editora: novo_ideditora,
+          id_editora: this.editora,
           nome: this.novo_livro,
           id_autor: novo_idautor,
           id_cartegoria: novo_idcartegoria,
+          preco_livro: "R$ " + this.novo_preco,
         });
         this.novo_livro = "";
+        this.novo_preco = "";
+        this.editora = "";
+        this.autor = "";
+        this.cartegoria = "";
       }
     },
     excluir(livro) {
@@ -70,9 +87,42 @@ export default {
         v-model="novo_livro"
         type="text"
         placeholder="Nome do livro"
-        class="input-maior"
+        class="input-menor"
       />
-
+      <select name="editoras" class="input-menor" v-model="editora">
+        <option value="630c1d03-cb59-460a-89b8-cca1d42c1bc6">
+          Companhia das Letras
+        </option>
+        <option value="597ceef8-36ac-4b0e-859e-77551c9818c0">
+          Editora Rocco
+        </option>
+        <option value="8ecb8a1e-5114-496d-9bee-1308dccf4b87">
+          Editora Arqueiro
+        </option>
+      </select>
+      <select name="cartegorias" class="input-menor" v-model="cartegoria">
+        <option value="16390338-fb77-4e4c-84f9-da9d1a1bdee7">Drama</option>
+        <option value="933f3ba7-0a49-4e63-80c3-134600987018">Medo</option>
+        <option value="ed4a44b6-720b-4b7b-a4fa-a2ff27ec3d17">Ficção</option>
+      </select>
+      <select name="autor" class="input-menor" v-model="autor">
+        <option value="4b95ec3f-c3a0-49d9-971d-b3696657fbbd">
+          Andrzej Sapkowski
+        </option>
+        <option value="ec38e964-9ddc-4d39-b6e3-688e239f8da9">
+          RONALD HUTTON
+        </option>
+        <option value="d48aca58-0c61-46fc-896f-9ea630a90cfb">
+          Antoine de Saint-Exupéry
+        </option>
+      </select>
+      <input
+        @keyup.enter="salvar"
+        v-model="novo_preco"
+        type="number"
+        placeholder="Preco do livro"
+        class="input-menor"
+      />
       <button @click="salvar">Salvar</button>
     </div>
     <div class="list-editoras">
@@ -84,6 +134,7 @@ export default {
             <th>Autor-id</th>
             <th>Cartegoria-id</th>
             <th>Editora-Id</th>
+            <th>Preço</th>
             <th>Ações</th>
           </tr>
         </thead>
@@ -103,6 +154,9 @@ export default {
             </td>
             <td>
               {{ livro.id_editora }}
+            </td>
+            <td>
+              {{ livro.preco_livro }}
             </td>
             <td>
               <button @click="editar(livro)">Editar</button>
